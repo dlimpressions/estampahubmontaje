c5d6b59244ee9717e368f6ae9cb87f5fd4b68319
dlimpressions <restsaborvallenato@gmail.com>
2026-04-02 09:19:20 -0500

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -450,7 +450,6 @@ function render() {
     const url   = d.url;
     const isSel = selectedItems.has(url);
     let score = vsScores.get(url);
-    let score = vsScores.get(url);
 let similarityPercent = 0;
 if (score !== undefined && score < 998) {
   if (score < 0.05) {
@@ -461,6 +460,7 @@ if (score !== undefined && score < 998) {
     similarityPercent = Math.round(similarity * 100);
   }
 }
+
      const item = document.createElement('div');
     item.className    = 'gal-item';
     item.dataset.url  = url;
